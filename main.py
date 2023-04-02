98335076351ff8486fd549b978ace530321820c0
sagar160589 <sagar.pndt305@gmail.com>
2023-04-02 15:00:01 +0530

    Commit for session related issue

--- main.py
+++ main.py
@@ -58,6 +58,8 @@ def home_page():
     all_blogs = Post.query.all()
     if 'secret_key' not in session:
         session['secret_key'] = os.environ.get('SECRET_KEY')
+        app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
     if 'user_id' not in session:
         return render_template('index.html', blogs=all_blogs, is_logged_in=False)
     return render_template('index.html', blogs=all_blogs, is_logged_in=current_user.is_authenticated)
